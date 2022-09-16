1657c60901852db8ff7d81dbf3e9ad3e964c85c7
Joshuapdelfin <109237663+joshua-delfin@users.noreply.github.com>
2022-09-16 13:04:49 -0400

    Reset help until further notice

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,7 @@
-const { MessageAttachment } = require("discord.js")
-
 module.exports = {
     name: 'help',
     description: "This is a help page!", 
     execute(message, args){
-        //message.channel.send("Current List of Commands: \n\n!help :: (You're here!) List of bot commands! \n!ping ::  Test command for bot to return text \n!random :: Generates a number from 0-100\n!cursed :: some cursed JP!\n");
-        message.channel.send(MessageAttachment("help.txt"));
+        message.channel.send("Current List of Commands: \n\n!help :: (You're here!) List of bot commands! \n!ping ::  Test command for bot to return text \n!random :: Generates a number from 0-100\n!cursed :: some cursed JP!\n");
     }
 }
