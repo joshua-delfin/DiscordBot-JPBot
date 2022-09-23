b481a0f5d05d6f2578d287b32633ec5a5291855f
Joshuapdelfin <109237663+joshua-delfin@users.noreply.github.com>
2022-09-23 10:06:20 -0400

    Connect mongo adjustment

--- mongo.js
+++ mongo.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const { mongoPath } = require ('./config.json');
 
 module.exports = async () => {
-    await mongoose.connect(mongoPath);
+    await mongoose.connect(mongoPath, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     return mongoose;
 
 }
